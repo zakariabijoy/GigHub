771a68644dff9b3b039d3c6659b17dbc14f6d5ce
zakariabijoy <zakaria.bijoy@live.com>
2019-12-24 04:22:11 +0600

    enable migrations & add migration for initial model

--- GigHub/Models/IdentityModels.cs
+++ GigHub/Models/IdentityModels.cs
@@ -1,8 +1,7 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace GigHub.Models
 {
@@ -21,7 +20,7 @@ namespace GigHub.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("GigHubDbContext", throwIfV1Schema: false)
         {
         }
 
