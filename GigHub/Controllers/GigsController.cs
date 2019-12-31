e9b8f6968525c585e8c446cd631f9cb91490a0fe
zakariabijoy <zakaria.bijoy@live.com>
2020-01-01 04:18:31 +0600

    Validation is added to GigFormViewModel

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -31,11 +31,16 @@ namespace GigHub.Controllers
         [HttpPost]
         public ActionResult Create(GigFormViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
             var gig = new Gig()
             {
                 ArtistId = User.Identity.GetUserId(),
                 Venue = viewModel.Venue,
-                Date = viewModel.DateTime,
+                Date = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre
 
             };
