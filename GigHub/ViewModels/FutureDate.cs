b21854109152b64ce8f6757ff265dd0cbe606096
zakariabijoy <zakaria.bijoy@live.com>
2020-01-02 02:48:02 +0600

    Implement custome validation (server side) and client side validation to Create A Gig Form

---
+++ GigHub/ViewModels/FutureDate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GigHub.ViewModels
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime datetime;
+          var isValid= DateTime.TryParseExact(Convert.ToString(value), 
+              "dd-MMM-yyyy", 
+              CultureInfo.CurrentCulture,
+              DateTimeStyles.None,
+              out datetime);
+          return (isValid && datetime > DateTime.Now);
+        }
+    }
+}
