4f12915748ecc6cfe546cf3ffb8d150e54d8c2e6
zakariabijoy <zakaria.bijoy@live.com>
2020-01-03 02:46:46 +0600

    Improve look and feel for home page

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace GigHub.Controllers
         {
             var upcomingGigs = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g=>g.Genre)
                 .Where(g => g.Date > DateTime.Now);
             return View(upcomingGigs);
         }
