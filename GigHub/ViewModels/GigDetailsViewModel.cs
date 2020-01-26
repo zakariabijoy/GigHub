4d88dc63e9ca9e5d349dc4a3d9829741aa348fb0
zakariabijoy <zakaria.bijoy@live.com>
2020-01-27 03:19:15 +0600

    implement gig details

---
+++ GigHub/ViewModels/GigDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GigHub.Models;
+
+namespace GigHub.ViewModels
+{
+    public class GigDetailsViewModel
+    {
+        public Gig Gig { get; set; }
+        public bool IsFollowing { get; set; }
+        public bool IsAttending { get; set; }   
+
+    }
+}
